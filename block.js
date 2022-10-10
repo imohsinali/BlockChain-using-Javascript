4d8a887fb400fd6fffab3e6b98aeb557cc93dcc4
Mohsin Ali <59974279+imohsinali@users.noreply.github.com>
2022-10-10 19:54:07 +0500

    Initial commit

---
+++ block.js
@@ -0,0 +1,11 @@
+class Block{
+    constructor(timestamp,prevHash,hash,data){
+        this.timestamp=timestamp
+        this.prevHash=prevHash
+        this.prevHash=prevHash
+        this.data=data
+    }
+}
+
+
+const block1=new Block("2/09/2022",'0xabce',"0xc123","Hello World")
