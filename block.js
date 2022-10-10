09d51bf0fb3a5bb54babf5a4cc1dae1af6d4fb13
Mohsin Ali <59974279+imohsinali@users.noreply.github.com>
2022-10-10 20:12:06 +0500

    Update block.js

--- block.js
+++ block.js
@@ -1,11 +1,19 @@
+const {GENESIS_DATA}= require("./config")
+
 class Block{
-    constructor(timestamp,prevHash,hash,data){
-        this.timestamp=timestamp
+    constructor({prevHash,hash,data}){
+        this.timestamp=new Date()
         this.prevHash=prevHash
-        this.prevHash=prevHash
+        this.hash=hash
         this.data=data
+    }
+    static genesis(){
+        return new this(GENESIS_DATA)
     }
 }
 
 
-const block1=new Block("2/09/2022",'0xabce',"0xc123","Hello World")
+const block1=new Block({data:"Mohsin",prevHash:'0x123',hash:"0x1244"})
+// console.log(block1)
+const genesisBlock=Block.genesis()
+console.log(genesisBlock)
