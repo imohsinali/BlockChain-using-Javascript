63d955ac837b708a310ab3ee415d006a948da53d
Mohsin Ali <59974279+imohsinali@users.noreply.github.com>
2022-10-11 23:17:27 +0500

    Update block.js

--- block.js
+++ block.js
@@ -2,8 +2,8 @@ const {GENESIS_DATA}= require("./config")
 const cryptoHash =require('./cryptoHash')
 
 class Block{
-    constructor({prevHash,hash,data}){
-        this.timestamp=new Date()
+    constructor({ timestamp, prevHash,hash,data}){
+        this.timestamp=timestamp
         this.prevHash=prevHash
         this.hash=hash
         this.data=data
@@ -14,20 +14,21 @@ class Block{
     static mineBlock({prevBlock,data}){
         const timestamp=new Date()
         const prevHash= prevBlock.hash
+        // console.log(prevBlock)
         return new this({
-            timestamp,
+            timestamp:new Date(),
             prevHash,
             data,
             hash:cryptoHash(timestamp,prevHash,data)
     })
     }
 }
-
-
 const block1=new Block({data:"Mohsin",prevHash:'0x123',hash:"0x1244"})
-// console.log(block1)
-const genesisBlock=Block.genesis()
-// console.log(genesisBlock)
 
-const result=Block.mineBlock({prevBlock:block1,data:"Mohsin"})
-console.log(result)
+module.exports=Block
+// // console.log(block1)
+// const genesisBlock=Block.genesis()
+// // console.log(genesisBlock)
+
+// const result=Block.mineBlock({prevBlock:block1,data:"Mohsin"})
+// console.log(result)
