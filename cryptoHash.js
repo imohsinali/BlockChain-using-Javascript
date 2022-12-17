067e78a4bad2aa19a8ab20393eda1cd2a05a0139
Mohsin Ali <59974279+imohsinali@users.noreply.github.com>
2022-12-17 11:41:00 +0500

    Update cryptoHash.js

--- cryptoHash.js
+++ cryptoHash.js
@@ -1,5 +1,5 @@
-const crypto=require('crypto')
-const cryptoHash=(...inputs)=>{
+import crypto from 'crypto'
+export const cryptoHash=(...inputs)=>{
     const hash=crypto.createHash('sha256')
     hash.update(inputs.sort().join(''))
     return hash.digest('hex')
@@ -8,5 +8,3 @@ const cryptoHash=(...inputs)=>{
 
 // const result=cryptoHash("hello","word")
 // console.log(result)
-
-module.exports=cryptoHash
